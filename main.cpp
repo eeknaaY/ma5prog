7a372750d140743e02eeffeb1ccf8fd68b43711b
Yankee <johanneshageus@hotmail.com>
2025-03-21 20:24:58 +0100

    d

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(){
     // Idk at this point
     // New Branch. Again. Again
+
+    // Newer branch
+
     const std::string albet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     std::string strStartNumber;
     int startNumberBase;
